b13ad6c3124408132b194924661ccad89bc3636b
Fakhrislamov Daniil <fahrislamovdaniilalekseevich@gmail.com>
2025-10-15 00:07:54 +0400

    Добавлен базовый интерфейс и функционал редактирования графа: добавление вершин, создание связи, удаление элементов.

--- src/main/java/com/App.java
+++ src/main/java/com/App.java
@@ -1,16 +1,46 @@
 package com;
 
+import com.controllers.Controller;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 /**
  * App
- * 	Grid (передаем координату, там внутри он смотрит очередь и обрабатывает клик как желает нужно по типу Event)
- * 		Map<Coordinate, GraphObject>
- * 		EventQueue
- * 	Panel (генерирует Event-ы в очередь событий)
- * 		List<Button> (вершина, дуга, текст)
- * 		EventQueue
+ * Grid (передаем координату, там внутри он смотрит очередь и обрабатывает клик как желает нужно по типу Event)
+ * Map<Coordinate, GraphObject>
+ * EventQueue
+ * Panel (генерирует Event-ы в очередь событий)
+ * List<Button> (вершина, дуга, текст)
+ * EventQueue
  */
-public class App {
-  public static void main(String[] args) {
-    System.out.println("Hello World!");
-  }
+public class App extends Application {
+
+    @SuppressWarnings("unused")
+    public static void main(String[] args) {
+        launch();
+    }
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("main-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 1024, 768);
+        stage.setTitle("Редактор графа");
+        stage.setScene(scene);
+        stage.show();
+
+        initGraph(fxmlLoader);
+    }
+
+    private void initGraph(FXMLLoader fxmlLoader) {
+        if (fxmlLoader.getController() instanceof Controller) {
+            ((Controller) fxmlLoader.getController()).initGraphView();
+        } else {
+            System.out.println("Exception!!! Controller can't init graph view.");
+        }
+    }
+
 }
---
+++ src/main/java/com/controllers/Controller.java
@@ -0,0 +1,190 @@
+package com.controllers;
+
+import com.brunomnsilva.smartgraph.graph.*;
+import com.brunomnsilva.smartgraph.graphview.*;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.TextInputDialog;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
+
+import java.net.URL;
+import java.util.*;
+import java.util.function.Consumer;
+
+public class Controller implements Initializable {
+
+    @FXML
+    private HBox graphBox;
+
+    private SmartGraphPanel<String, String> graphView;
+
+    private final List<SmartStylableNode> selected = new ArrayList<>();
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        createGraphView();
+    }
+
+    @FXML
+    private void onAddVertex() {
+        showTextInputDialog(
+                "Создание новой вершины",
+                "Введите название новой вершины",
+                name -> {
+                    try {
+                        graphView.getModel().insertVertex(name);
+                    } catch (InvalidVertexException _ignored) {
+                        showAlert("Вершина с данным именем уже существует!");
+                    }
+
+                    new ArrayList<>(selected).forEach(this::select);
+                    graphView.update();
+                }
+        );
+    }
+
+    @FXML
+    private void onEditSelected() {
+        if (selected.size() == 1) {
+            showTextInputDialog(
+                    "Изменение",
+                    "Введите новое значение",
+                    value -> {
+                        // Library bug with replace
+//                          if (item instanceof SmartGraphVertex) {
+//                              Vertex<String> vertex = ((SmartGraphVertex<String>) item).getUnderlyingVertex();
+//                              graphView.getModel().replace(vertex, value);
+//                          } else if (item instanceof SmartGraphEdge) {
+//                              Edge<String, String> edge = ((SmartGraphEdge<String, String>) item).getUnderlyingEdge();
+//                              graphView.getModel().replace(edge, value);
+//                          }
+
+//                          new ArrayList<>(selected).forEach(this::select);
+//                          graphView.update();
+                    }
+            );
+        } else {
+            showAlert("Выберите ровно один элемент для редактирования!");
+        }
+    }
+
+    @FXML
+    private void onConnectSelected() {
+        if (selected.size() == 2
+                && selected.stream().allMatch(item -> item instanceof SmartGraphVertex)
+        ) {
+            showTextInputDialog(
+                    "Создание связи",
+                    "Введите вес новой связи",
+                    value -> {
+                        //noinspection unchecked
+                        graphView.getModel().insertEdge(
+                                ((SmartGraphVertex<String>) selected.get(0)).getUnderlyingVertex(),
+                                ((SmartGraphVertex<String>) selected.get(1)).getUnderlyingVertex(),
+                                value
+                        );
+
+                        new ArrayList<>(selected).forEach(this::select);
+                        graphView.update();
+                    }
+            );
+        } else {
+            showAlert("Выберите ровно две вершины для соединения!");
+        }
+    }
+
+    @FXML
+    @SuppressWarnings("unchecked")
+    private void onRemoveSelected() {
+        selected.forEach(item -> {
+            if (item instanceof SmartGraphVertex) {
+                graphView.getModel().removeVertex(((SmartGraphVertex<String>) item).getUnderlyingVertex());
+            } else if (item instanceof SmartGraphEdge) {
+                graphView.getModel().removeEdge(((SmartGraphEdge<String, String>) item).getUnderlyingEdge());
+            }
+        });
+
+        new ArrayList<>(selected).forEach(this::select);
+        graphView.update();
+    }
+
+    public void initGraphView() {
+        if (graphView != null) {
+            graphView.init();
+        } else {
+            System.out.println("Exception!!! Scene has not yet been initialized.");
+        }
+    }
+
+    private void createGraphView() {
+        SmartPlacementStrategy strategy = new SmartCircularSortedPlacementStrategy();
+        graphView = new SmartGraphPanel<>(getDefaultGraph(), strategy);
+
+        graphView.setVertexDoubleClickAction(this::select);
+        graphView.setEdgeDoubleClickAction(this::select);
+
+        HBox.setHgrow(graphView, Priority.ALWAYS);
+        graphBox.getChildren().add(graphView);
+    }
+
+    private Graph<String, String> getDefaultGraph() {
+        Digraph<String, String> d = new DigraphEdgeList<>();
+        Vertex<String> vA = d.insertVertex("A");
+        Vertex<String> vB = d.insertVertex("B");
+        Vertex<String> vC = d.insertVertex("C");
+        Vertex<String> vD = d.insertVertex("D");
+
+        d.insertEdge(vA, vB, "AB");
+        d.insertEdge(vB, vC, "BC");
+        d.insertEdge(vC, vD, "CD");
+        d.insertEdge(vA, vD, "AD");
+        d.insertEdge(vA, vC, "AC");
+
+        return d;
+    }
+
+    private void select(SmartStylableNode item) {
+        if (selected.contains(item)) {
+            selected.remove(item);
+            item.setStyleClass(getDefaultStyleClass(item));
+        } else {
+            selected.add(item);
+            item.setStyleClass(getDefaultStyleClass(item) + "-selected");
+        }
+    }
+
+    private String getDefaultStyleClass(SmartStylableNode item) {
+        if (item instanceof SmartGraphVertex) {
+            return "vertex";
+        } else if (item instanceof SmartGraphEdge) {
+            return "edge";
+        } else {
+            return "";
+        }
+    }
+
+    private void showTextInputDialog(
+            String title,
+            String header,
+            Consumer<String> action
+    ) {
+        TextInputDialog dialog = new TextInputDialog("");
+        dialog.setTitle(title);
+        dialog.setHeaderText(header);
+        dialog.setContentText("");
+
+        Optional<String> result = dialog.showAndWait();
+        result.ifPresent(action);
+    }
+
+    private void showAlert(String header) {
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle("Ошибка!");
+        alert.setHeaderText(header);
+        alert.setContentText("");
+        alert.showAndWait();
+    }
+
+}
